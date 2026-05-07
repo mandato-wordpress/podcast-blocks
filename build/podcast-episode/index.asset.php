cda066c7f343670d0c8568f88937bc05941cd6e7
Angelo Mandato <3031332+angelomandato@users.noreply.github.com>
2026-05-06 23:25:52 -0400

    Latest version of block compiled

---
+++ build/podcast-episode/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '745b211f50b2028fbfe6');
---
+++ build/podcast-episode/view.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => '7745a683a93ae7ed2359');
