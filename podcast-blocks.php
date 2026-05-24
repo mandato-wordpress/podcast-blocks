20fc047ffa21737ff1162affd577bab32360f361
Angelo Mandato <angelo@mandato.com>
2026-05-24 17:10:00 -0400

    Update podcast-blocks.php
    Fixed tabs

--- podcast-blocks.php
+++ podcast-blocks.php
@@ -5,7 +5,7 @@
  Description:       Create and manage podcast episodes using Gutenberg blocks. Includes Apple Podcasts-compatible RSS feed.
  Version:           0.9.3
  Requires at least: 6.5
- Tested up to:	 	7.0
+ Tested up to:      7.0
  Requires PHP:      8.1
  Author:            Angelo Mandato
  License:           GPL-2.0-or-later
